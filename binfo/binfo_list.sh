7a7e3d35b3f390792830e829d23b0779fa77ef4e
Chen Yufei <cyfdecyf@gmail.com>
2024-08-03 19:11:56 +0800

    Skip projects can't compile.

--- binfo/binfo_list.sh
+++ binfo/binfo_list.sh
@@ -21,12 +21,12 @@ LIST_BINFO_FILE_BASENAME=(
     "009_04_hipcc_clr.binfo"
     "010_01_rocPRIM.binfo"
     "010_02_rocthrust.binfo"
-    #"011_01_rocprofiler.binfo"
+    #"011_01_rocprofiler.binfo"  # Contains x86 specific code.
     "011_02_roctracer.binfo"
     "012_01_rocdbgapi.binfo"
     "012_02_rocgdb.binfo"
     "013_01_rocm_smi_lib.binfo"
-    #"013_02_amdsmi.binfo"
+    #"013_02_amdsmi.binfo"  # Contains x86 specific code.
     "013_03_nvtop.binfo"
     "014_01_hwloc.binfo"
     "014_02_boost.binfo"
@@ -36,37 +36,39 @@ LIST_BINFO_FILE_BASENAME=(
     "015_02_ucc_openmpi.binfo"
     "015_03_openmpi.binfo"
     "016_01_flang_libpgmath.binfo"
-    "016_02_flang.binfo"
+    #"016_02_flang.binfo"  # Depends on quadmath which does not support AArch64.
     "016_03_llvm_project_openmp.binfo"
-    "016_04_flang_runtime.binfo"
-    "016_05_llvm_project_openmp_fortran.binfo"
+    #"016_04_flang_runtime.binfo"
+    #"016_05_llvm_project_openmp_fortran.binfo"
     "017_hipify.binfo"
     "018_rccl.binfo"
     "019_clang-ocl.binfo"
-    "020_01_amd_fftw_single_precision.binfo"
-    "020_02_amd_fftw_double_precision.binfo"
-    "020_03_amd_fftw_long_double_precision.binfo"
-    "020_04_amd_fftw_quad_precision.binfo"
+    #"020_01_amd_fftw_single_precision.binfo" # Optimized for AMD processors, does not work on AArch64.
+    #"020_02_amd_fftw_double_precision.binfo"
+    #"020_03_amd_fftw_long_double_precision.binfo"
+    #"020_04_amd_fftw_quad_precision.binfo"
     "021_rocFFT.binfo"
     "022_01_rocSOLVER_fmt.binfo"
     "022_02_rocSOLVER_lapack.binfo"
     "022_03_blis.binfo"
     "022_04_OpenBLAS.binfo"
     "022_05_libflame.binfo"
+    # I installed this by executing "pip install ." in project directory to make
+    # rocBLAS able to find this library.
     "023_01_rocBLAS_Tensile.binfo"
     "023_02_rocBLAS.binfo"
     "023_03_rocSPARSE.binfo"
     "023_04_SuiteSparse.binfo"
     "024_01_rocSOLVER.binfo"
     "024_02_hipSOLVER.binfo"
-    "025_01_hipBLAS.binfo"
-    "025_02_hipBLASLt.binfo"
+    #"025_01_hipBLAS.binfo"  # requires flang
+    #"025_02_hipBLASLt.binfo"  # requires flang
     "025_03_blaze.binfo"    
     "026_01_hiprand_rocrand.binfo"
     "026_02_hiprand_hiprand.binfo"
-    "027_hipFFT.binfo"
-    "028_01_hipSPARSE.binfo"
-    "028_02_hipSPARSELt.binfo"
+    #"027_hipFFT.binfo"  # requires amd-fftw
+    #"028_01_hipSPARSE.binfo"  # Compile error, maybe not compatible with Tensile version.
+    #"028_02_hipSPARSELt.binfo"
     "029_rocALUTION.binfo"
     "030_hipCUB.binfo"
     "031_01_miopen_rocMLIR.binfo"
@@ -76,11 +78,11 @@ LIST_BINFO_FILE_BASENAME=(
     "032_03_frugally_deep.binfo"
     "033_01_composable_kernel.binfo"
     "033_02_composable_kernel_jit.binfo"
-    "034_miopen.binfo"
-    "035_AMDMIGraphX.binfo"
+    #"034_miopen.binfo"  # Compile error, short4 data type etc.
+    #"035_AMDMIGraphX.binfo"  # requires miopen
     "036_rocWMMA.binfo"
-    "037_magma.binfo"
-    "038_aotriton.binfo"
+    #"037_magma.binfo"  # requires flang
+    #"038_aotriton.binfo"  # downloading googletest and https://tritonlang.blob.core.windows.net/llvm-builds/llvm-49af6502-ubuntu-x64.tar.gz, timeout
     "039_01_pytorch_dependencies.binfo"
     "039_02_pytorch.binfo"
     "039_03_pytorch_vision.binfo"
